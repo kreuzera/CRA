b60f239534a65be00338ab365567215d693c5ea5
Patrick D <pdohmen@smail.uni-koeln.de>
2015-05-15 18:25:03 +0200

    restrukturiert, neuen Tager eingebaut

--- Hauptseminar/src/FIltering/NPFilter.java
+++ Hauptseminar/src/FIltering/NPFilter.java
@@ -1,38 +1,52 @@
 package FIltering;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
 import java.util.Set;
-import net.didion.jwnl.util.factory.Param;
+
 import opennlp.tools.cmdline.parser.ParserTool;
+import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.parser.ParserFactory;
 import opennlp.tools.parser.ParserModel;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.InvalidFormatException;
 import opennlp.tools.parser.Parse;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 
 public class NPFilter {
 
 	
     static Set<String> nounPhrases = new HashSet<String>();
-    static String testSentence = "Half an ancient silver fifty cent piece, several quotations from John Donne's sermons written incorrectly, each on a separate piece of transparent tissue-thin paper. The lazy brown fox jumps over the fence. That fence was build by your mother.";
+    static String testSentence = "the quick brown fox jumps over the lazy dog";//"Half an ancient silver fifty cent piece, several quotations from John Donne's sermons written incorrectly, each on a separate piece of transparent tissue-thin paper. The lazy brown fox jumps over the fence. That fence was build by your mother.";
     static ParserModel chunkingModel;
     static SentenceModel sentenceModel;
+    static POSModel taggerModel;
+    static TokenizerModel tokenizerModel;
     private static NPFilter instance = null;
 
+    static double loadInTime;
+    static double parseTime; 
+    private static ArrayList<String[]> wordList = new ArrayList<String[]>();
+    
     
     protected NPFilter(){
-    	try {
-            sentenceModel = new SentenceModel(new FileInputStream("en-sent.bin"));
-			chunkingModel = new ParserModel(new FileInputStream("en-parser-chunking.bin"));
+    	/*try {
+    		loadInTime = System.currentTimeMillis();
+            
+			
+			System.out.println("Time to Load Models: "+(System.currentTimeMillis() - loadInTime)/1000);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 
     }
     
@@ -47,11 +61,38 @@ public class NPFilter {
     	NPDetect(testSentence);
     	return nounPhrases;
     }
+    /**
+     * returns an ArrayList of Tagged Words from a sentence
+     * For a documentation of tags see 
+     * http://blog.dpdearing.com/2011/12/opennlp-part-of-speech-pos-tags-penn-english-treebank/
+     * @param sentence
+     * @return
+     */
+    public static ArrayList<String[]> GetTaggedWordsFromSentence(String sentence){
+		try {
+			wordList = WordTagger(WordTokenizer(sentence));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return wordList;
+    	
+    }
+    
     
     public static void main(String args[]){
-    	//NPFilter bla = new NPFilter();
     	getInstance();
-    	GetNounPhrases(testSentence);
+    	parseTime = System.currentTimeMillis();
+    	//GetNounPhrases(testSentence);
+    	try {    			
+    		for(String[] s : WordTagger(WordTokenizer(testSentence))){
+    			System.out.println(s[0] + " "+ s[1]);
+    		}
+    			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	System.out.println("Time to actually Parse Sentence: "+(System.currentTimeMillis() - parseTime)/1000);
     }
 
     /**
@@ -61,11 +102,10 @@ public class NPFilter {
 	private static void NPDetect(String input){
 		try{
     		String[] sentences = SentenceTokenizer(testSentence);
-    		//String[] sentences = sentenceTokenizer(input);
     		for(String s : sentences)
     			Parse(s);
-    		for (String s : nounPhrases)
-        		System.out.println(s);       	
+    		//for (String s : nounPhrases)
+        		//System.out.println(s);       	
     	}
     	catch(Exception e){
     		e.printStackTrace();
@@ -79,8 +119,11 @@ public class NPFilter {
 	 * @throws IOException
 	 */
 	private static void Parse(String sentence) throws InvalidFormatException, IOException{
-    	opennlp.tools.parser.Parser parser = ParserFactory.create(chunkingModel);
+    	if(chunkingModel == null)
+			chunkingModel = new ParserModel(new FileInputStream("en-parser-chunking.bin"));
+		opennlp.tools.parser.Parser parser = ParserFactory.create(chunkingModel);
     	opennlp.tools.parser.Parse[] toParses = ParserTool.parseLine(sentence, parser, 1);
+    	
     	for(Parse p: toParses)
     		ExtractNPs(p);
     	/*for(Parse p: toParses)
@@ -96,9 +139,23 @@ public class NPFilter {
 	    if (parse.getType().equals("NP")) {
 	         nounPhrases.add(parse.getCoveredText());
 	    }
-	    for (Parse child : parse.getChildren())
+	    for (Parse child : parse.getChildren()){
 	    	ExtractNPs(child);
+	    }
 	}
+
+	public static ArrayList<String[]> WordTagger(String[] words) throws IOException {
+		if(taggerModel==null)
+			taggerModel = new POSModelLoader().load(new File("en-pos-maxent.bin"));
+		POSTaggerME tagger = new POSTaggerME(taggerModel);
+		String[] tags = null;
+		tags = tagger.tag(words);
+			for(int i= 0; i< tags.length; i++){
+				String[] temp = {tags[i], words[i]};
+				wordList.add(temp);
+			}
+		return wordList;
+		}
 	
     /**
      * uses OpenNLP to split sentences from a String
@@ -107,10 +164,17 @@ public class NPFilter {
      * @throws IOException
      */
 	private static String[] SentenceTokenizer(String text)throws IOException{
-        
+		if(sentenceModel==null)
+			sentenceModel = new SentenceModel(new FileInputStream("en-sent.bin"));
         SentenceDetectorME sDetector = new SentenceDetectorME(sentenceModel);
-        //is.close();
         return sDetector.sentDetect(text);
     }
+	
+	public static String[] WordTokenizer(String sentence) throws InvalidFormatException, IOException {
+		if(tokenizerModel==null)
+			tokenizerModel = new TokenizerModel(new FileInputStream("en-token.bin"));
+		Tokenizer tokenizer = new TokenizerME(tokenizerModel);
+		return tokenizer.tokenize(sentence);
+	}
 	
 }
