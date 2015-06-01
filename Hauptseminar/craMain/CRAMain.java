f51c65656cbbbda4f0061ac87b7bd256bd5077d4
Kai <kaiaugustin@web.de>
2015-06-01 18:31:11 +0200

    removed unnecessary imports

--- Hauptseminar/craMain/CRAMain.java
+++ Hauptseminar/craMain/CRAMain.java
@@ -1,20 +1,12 @@
 package craMain;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.ParserConfigurationException;
 
 import model.Element;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import fileTransfer.FileReader;
 import filtering.NPFilter;
