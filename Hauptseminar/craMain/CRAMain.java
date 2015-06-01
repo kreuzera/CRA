5dfc727ca24f74736ee34535c9f71bdcd36c01ef
Kai <kaiaugustin@web.de>
2015-06-01 20:26:20 +0200

    Merge added Switched to HashMap

--- Hauptseminar/craMain/CRAMain.java
+++ Hauptseminar/craMain/CRAMain.java
@@ -1,8 +1,11 @@
 package craMain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
+
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import model.Element;
 
@@ -18,18 +21,21 @@ public class CRAMain {
 		NodeList abstracts = fileReader.getAbstracts("testFile/ebscohost export.xml");
 		
 		NPFilter npfilter = new NPFilter();
+		npfilter.GetTaggedWordsFromSentence("");
 		
 		ArrayList<String[]> taggedWordList = new ArrayList<String []>();
-		LinkedList<Element> nounPhrases = new LinkedList<Element>();
+		HashMap<String, LinkedList<Element>> nounPhrases = new HashMap<String, LinkedList<Element>>();
 		LinkedList<Element> lastElements = new LinkedList<Element>();
 		Element tempElement = null;
+		System.out.print("Tagging and linking ...");
+		long start = System.currentTimeMillis();
 		for (int i = 0; i<abstracts.getLength(); i++){
 //			System.out.println(abstracts.item(i).getTextContent());
 			taggedWordList = npfilter.GetTaggedWordsFromSentence(abstracts.item(i).getTextContent());
 			
 			for(String[] s : taggedWordList){
-				if(s[0].equals("NN")||s[0].equals("NNS")||s[0].equals("NNP")||s[0].equals("JJ")||s[0].equals("JJR")||s[0].equals("JJS")){
-					lastElements.add(new Element(s[1]));
+				if(s[0].equals("NN")||s[0].equals("NNS")||s[0].equals("NNP")||s[0].equals("JJ")||s[0].equals("JJR")||s[0].equals("JJS")){					
+					lastElements.add(new Element(s[1].toLowerCase()));
 				}else{
 					if(!lastElements.isEmpty()){
 						if(tempElement!=null){
@@ -41,7 +47,14 @@ public class CRAMain {
 								e.addNeighbour(n);								
 							}							
 							e.getNeighbour().remove(e);
-							nounPhrases.add(e);
+							if(nounPhrases.get(e.getNounPhrase())!=null){
+								nounPhrases.get(e.getNounPhrase().toLowerCase()).add(e);
+							}else{
+								LinkedList<Element>tempList = new LinkedList<Element>();
+								tempList.add(e);
+								nounPhrases.put(e.getNounPhrase().toLowerCase(), tempList);
+							}
+								
 						}
 
 						tempElement = lastElements.getLast();
@@ -57,13 +70,38 @@ public class CRAMain {
 			lastElements.clear();
 			
 		}
-		for(Element e: nounPhrases){
-			String test = e.getNounPhrase()+": ";
-			for(Element n: e.getNeighbour()){
-				test += n.getNounPhrase()+", ";
+		System.out.println(" finished in "+(System.currentTimeMillis()-start)+"ms");
+		// Merge
+		System.out.print("Merging ...");
+		start = System.currentTimeMillis();
+		int i = 0;
+		for(LinkedList<Element> eList: nounPhrases.values()){
+			Element first = eList.getFirst();
+			for(Element e: eList){
+				i++;
+				if(e!=first){
+					for(Element n: e.getNeighbour()){
+						first.addNeighbour(n);
+					}					
+				}					
 			}
-			System.out.println(test);
 		}
+		System.out.println(" finished in "+(System.currentTimeMillis()-start)+"ms. "+i+" nouns merged into "+nounPhrases.size()+" nouns. Printing in 5 Seconds");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Print
+		for(LinkedList<Element> eList: nounPhrases.values()){
+			String testung = eList.getFirst().getNounPhrase()+": ";
+			for(Element n: eList.getFirst().getNeighbour()){
+				testung += n.getNounPhrase()+", ";
+			}
+			System.out.println(testung);
+		}
+
 
 	}
 	
