dbce045c1acb15913169a4707743f0421dcb6b32
Patrick D <pdohmen@smail.uni-koeln.de>
2015-06-20 17:25:31 +0200

    added Stanford NLP Sentence splitting

--- Hauptseminar/filtering/NPFilter.java
+++ Hauptseminar/filtering/NPFilter.java
@@ -5,11 +5,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
+import net.didion.jwnl.JWNLException;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.util.CoreMap;
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.coref.mention.JWNLDictionary;
 import opennlp.tools.parser.ParserFactory;
 import opennlp.tools.parser.ParserModel;
 import opennlp.tools.sentdetect.SentenceDetectorME;
@@ -21,45 +26,43 @@ import opennlp.tools.util.InvalidFormatException;
 import opennlp.tools.parser.Parse;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.process.Morphology;
 
 public class NPFilter {
 
-	
-    static Set<String> nounPhrases = new HashSet<String>();
-    static String testSentence = "the quick brown fox jumps over the lazy dog. Half an ancient silver fifty cent piece, several quotations from John Donne's sermons written incorrectly, each on a separate piece of transparent tissue-thin paper. The lazy brown fox jumps over the fence. That fence was build by your mother.";
-    static ParserModel chunkingModel;
-    static SentenceModel sentenceModel;
-    static POSModel taggerModel;
-    static TokenizerModel tokenizerModel;
-    public static MaxentTagger stanfordTagger;
-    private static NPFilter instance = null;
-    Morphology morph;
-    
-    static double loadInTime;
-    static double parseTime; 
-    private ArrayList<String[]> wordList = new ArrayList<String[]>();
-    
-   
-    public static NPFilter getInstance() {
-        if(instance == null) {
-           instance = new NPFilter();
-        }
-        return instance;
-     }
-    
-    public static Set<String> GetNounPhrases(String input){
-    	NPDetect(testSentence);
-    	return nounPhrases;
-    }
-    /**
-     * returns an ArrayList of Tagged Words from a sentence
-     * For a documentation of tags see 
-     * http://blog.dpdearing.com/2011/12/opennlp-part-of-speech-pos-tags-penn-english-treebank/
-     * @param sentence
-     * @return
-     */
-    public ArrayList<String[]> GetTaggedWordsFromSentence(String sentence){
+	static Set<String> nounPhrases = new HashSet<String>();
+	static ParserModel chunkingModel;
+	static SentenceModel sentenceModel;
+	static POSModel taggerModel;
+	static TokenizerModel tokenizerModel;
+	public static MaxentTagger stanfordTagger;
+	private static NPFilter instance = null;
+	private Morphology morph;
+	static double loadInTime;
+	static double parseTime;
+	private ArrayList<String[]> wordList = new ArrayList<String[]>();
+	private Properties props = null;
+	private StanfordCoreNLP pipe = null;
+	public static NPFilter getInstance() {
+		if (instance == null) {
+			instance = new NPFilter();
+		}
+		return instance;
+	}
+
+	/**
+	 * returns an ArrayList of Tagged Words from a sentence For a documentation
+	 * of tags see
+	 * http://blog.dpdearing.com/2011/12/opennlp-part-of-speech-pos-tags
+	 * -penn-english-treebank/
+	 * 
+	 * @param sentence
+	 * @return
+	 */
+	public ArrayList<String[]> GetTaggedWordsFromSentence(String sentence) {
 		try {
 			wordList.clear();
 			wordList = WordTagger(WordTokenizer(sentence));
@@ -67,120 +70,167 @@ public class NPFilter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	return wordList;
-    	
-    }    
+		return wordList;
 
-    
-    
-
-    /**
-     * 
-     * @param input
-     */
-	private static void NPDetect(String input){
-		try{
-    		String[] sentences = SentenceTokenizer(testSentence);
-    		for(String s : sentences)
-    			Parse(s);
-    		//for (String s : nounPhrases)
-        		//System.out.println(s);       	
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
 	}
-	
+
+	/**
+	 * 
+	 * @param input
+	 */
+//	private static void NPDetect(String input) {
+//		try {
+//			String[] sentences = SentenceTokenizer(testSentence);
+//			for (String s : sentences)
+//				Parse(s);
+//			// for (String s : nounPhrases)
+//			// System.out.println(s);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
 	/**
 	 * parses a String, tags its elements
+	 * 
 	 * @param sentence
 	 * @throws InvalidFormatException
 	 * @throws IOException
 	 */
-	private static void Parse(String sentence) throws InvalidFormatException, IOException{
-    	if(chunkingModel == null)
-			chunkingModel = new ParserModel(new FileInputStream("en-parser-chunking.bin"));
-		opennlp.tools.parser.Parser parser = ParserFactory.create(chunkingModel);
-    	opennlp.tools.parser.Parse[] toParses = ParserTool.parseLine(sentence, parser, 1);
-    	
-    	for(Parse p: toParses)
-    		ExtractNPs(p);
-    	/*for(Parse p: toParses)
-    		p.show();*/
-    }
-    
+	private static void Parse(String sentence) throws InvalidFormatException,
+			IOException {
+		if (chunkingModel == null)
+			chunkingModel = new ParserModel(new FileInputStream(
+					"en-parser-chunking.bin"));
+		opennlp.tools.parser.Parser parser = ParserFactory
+				.create(chunkingModel);
+		opennlp.tools.parser.Parse[] toParses = ParserTool.parseLine(sentence,
+				parser, 1);
+
+		for (Parse p : toParses)
+			ExtractNPs(p);
+		/*
+		 * for(Parse p: toParses) p.show();
+		 */
+	}
+
 	/***
-	 * recursively loops through parse tree and extracts
-	 * the elements that have been tagged as "NP" (or Noun Phrase)
+	 * recursively loops through parse tree and extracts the elements that have
+	 * been tagged as "NP" (or Noun Phrase)
+	 * 
 	 * @param parse
 	 */
 	private static void ExtractNPs(Parse parse) {
-	    if (parse.getType().equals("NP")) {
-	         nounPhrases.add(parse.getCoveredText());
-	    }
-	    for (Parse child : parse.getChildren()){
-	    	ExtractNPs(child);
-	    }
+		if (parse.getType().equals("NP")) {
+			nounPhrases.add(parse.getCoveredText());
+		}
+		for (Parse child : parse.getChildren()) {
+			ExtractNPs(child);
+		}
 	}
 
 	public ArrayList<String[]> WordTagger(String[] words) throws IOException {
-		if(taggerModel==null)
-			taggerModel = new POSModelLoader().load(new File("en-pos-maxent.bin"));
+		if (taggerModel == null)
+			taggerModel = new POSModelLoader().load(new File(
+					"en-pos-maxent.bin"));
 		POSTaggerME tagger = new POSTaggerME(taggerModel);
 		String[] tags = null;
 		tags = tagger.tag(words);
-			for(int i= 0; i< tags.length; i++){
-				String[] temp = {tags[i], words[i]};
-				if(temp[0].equals("NNS") || temp[0].equals("NNPS")){
-					temp[1] = lemmatize(temp[1], temp[0]);
-				}
-				wordList.add(temp);
+		for (int i = 0; i < tags.length; i++) {
+			String[] temp = { tags[i], words[i] };
+			if (temp[0].equals("NNS") || temp[0].equals("NNPS")) {
+				temp[1] = StanfordLemmatize(temp[1], temp[0]);
 			}
-		return wordList;
+			wordList.add(temp);
 		}
-	
-    /**
-     * uses OpenNLP to split sentences from a String
-     * @param text
-     * @return
-     * @throws IOException
-     */
-	private static String[] SentenceTokenizer(String text)throws IOException{
-		if(sentenceModel==null)
-			sentenceModel = new SentenceModel(new FileInputStream("en-sent.bin"));
-        SentenceDetectorME sDetector = new SentenceDetectorME(sentenceModel);
-        return sDetector.sentDetect(text);
-    }
-	
-	public static String[] WordTokenizer(String sentence) throws InvalidFormatException, IOException {
-		if(tokenizerModel==null)
-			tokenizerModel = new TokenizerModel(new FileInputStream("en-token.bin"));
+		return wordList;
+	}
+
+	/**
+	 * uses OpenNLP to split sentences from a String
+	 * 
+	 * @param text
+	 * @return
+	 * @throws IOException
+	 */
+	private static String[] SentenceTokenizer(String text) throws IOException {
+		if (sentenceModel == null)
+			sentenceModel = new SentenceModel(
+					new FileInputStream("en-sent.bin"));
+		SentenceDetectorME sDetector = new SentenceDetectorME(sentenceModel);
+		return sDetector.sentDetect(text);
+	}
+
+	public static String[] WordTokenizer(String sentence)
+			throws InvalidFormatException, IOException {
+		if (tokenizerModel == null)
+			tokenizerModel = new TokenizerModel(new FileInputStream(
+					"en-token.bin"));
 		Tokenizer tokenizer = new TokenizerME(tokenizerModel);
 		return tokenizer.tokenize(sentence);
 	}
-	
-	public ArrayList<String[]> StanfordNlpTagger(String sentence){
-		if(stanfordTagger == null){
-			stanfordTagger = new MaxentTagger("english-left3words-distsim.tagger");
+
+	public ArrayList<String[]> StanfordNlpTagger(String Text) {
+		if (stanfordTagger == null) {
+			stanfordTagger = new MaxentTagger(
+					"english-left3words-distsim.tagger");
 		}
 		wordList.clear();
-		sentence = sentence.replaceAll("\\u2010","-");
-		String tagged= stanfordTagger.tagString(sentence);
-		String[] words = tagged.split(" ");
-		for(String word: words){
-			String[] temp = word.split("_");
-			if(temp[1].equals("NNS") || temp[1].equals("NNPS")){
-				temp[0] = lemmatize(temp[0], temp[1]);
+		Text = Text.replaceAll("\\u2010", "-");
+		String[] sentences = StanfordSentenceSplitter(Text);
+		for (String sentence : sentences) {
+			String tagged = stanfordTagger.tagString(sentence);
+			String[] words = tagged.split(" ");
+
+			for (String word : words) {
+				String[] temp = word.split("_");
+				if (temp[1].equals("NNS") || temp[1].equals("NNPS")) {
+					temp[0] = StanfordLemmatize(temp[0], temp[1]);
+				}
+				wordList.add(new String[] { temp[1], temp[0] });
 			}
-			wordList.add(new String[]{temp[1],temp[0]});
 		}
 		return wordList;
 	}
-	
-	public String lemmatize (String word, String tag){
-		if(morph == null)
-			morph= new Morphology();
+
+	public String StanfordLemmatize(String word, String tag) {
+		if (morph == null)
+			morph = new Morphology();
 		return morph.lemma(word, tag);
 	}
-	
+
+	// not yet working
+	public String OpenNlpLemmatize(String word, String tag) {
+		JWNLDictionary dic = null;
+		String[] lemmas = null;
+		try {
+			dic = new JWNLDictionary(
+					"C:\\Users\\Densu\\Desktop\\SimilarityUtils\\WordNet-2.0\\dict\\");
+		} catch (IOException | JWNLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (dic != null) {
+			lemmas = dic.getLemmas(tag, word);
+		}
+		return null;
+	}
+
+	private String[] StanfordSentenceSplitter(String Text) {
+		if(props==null){
+			props = new Properties();
+			props.setProperty("annotators","tokenize, ssplit");
+		}
+		if(pipe == null) pipe = new StanfordCoreNLP(props);
+
+		Annotation importedText = new Annotation(Text);
+		pipe.annotate(importedText);
+		List<CoreMap> sentences = importedText.get(SentencesAnnotation.class);
+		String[] temp = new String[sentences.size()];
+		for (int i = 0; i < sentences.size(); i++) {
+			temp[i] = sentences.get(i).toString();
+		}
+		return temp;
+	}
+
 }
