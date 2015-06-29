aa6f9e01fc74e1ab524b2e8ab443481e618ffea8
Kai <kaiaugustin@web.de>
2015-06-29 18:48:33 +0200

    jetzt aber

--- Hauptseminar/cra/MainApp.java
+++ Hauptseminar/cra/MainApp.java
@@ -102,26 +102,30 @@ public class MainApp extends Application {
     }
     
     public void showRecordDetail(ResonanceTableClass res){
-        // Load the fxml file and create a new stage for the popup dialog.
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(MainApp.class.getResource("view/RecordDetail.fxml"));
-        AnchorPane page = (AnchorPane) loader.load();
-
-        // Create the dialog Stage.
-        Stage dialogStage = new Stage();
-        dialogStage.setTitle("Edit Person");
-        dialogStage.initModality(Modality.WINDOW_MODAL);
-        dialogStage.initOwner(primaryStage);
-        Scene scene = new Scene(page);
-        dialogStage.setScene(scene);
-
-        // Set the person into the controller.
-        RecordDetailController controller = loader.getController();
-        controller.setDialogStage(dialogStage);
-//        controller.setPerson(person);
-
-        // Show the dialog and wait until the user closes it
-        dialogStage.showAndWait();
+    	try {
+	        // Load the fxml file and create a new stage for the popup dialog.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainApp.class.getResource("view/RecordDetail.fxml"));
+	        AnchorPane page = (AnchorPane) loader.load();
+	
+	        // Create the dialog Stage.
+	        Stage dialogStage = new Stage();
+	        dialogStage.setTitle("Record");
+	        dialogStage.initModality(Modality.WINDOW_MODAL);
+	        dialogStage.initOwner(primaryStage);
+	        Scene scene = new Scene(page);
+	        dialogStage.setScene(scene);
+	
+	        // Set the person into the controller.
+	        RecordDetailController controller = loader.getController();
+	        controller.setDialogStage(dialogStage);
+	        controller.setRecord(res);
+	
+	        // Show the dialog and wait until the user closes it
+	        dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public Stage getPrimaryStage(){
